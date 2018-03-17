cb36d407c4d2862b7cf2f71f36aad6b4b9282020
nancyhenry89 <nancyhenry89@gmail.com>
2018-03-17 22:58:55 +0200

    My profile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -360,3 +360,45 @@ $(window).resize(function() {
       initSlider();
     }*/
 });
+
+
+
+
+
+//file uploader
+$("#uploadFile").change(function() {
+    var formData = new FormData();
+    formData.append('file', this.files[0]);
+
+    $("#files").append($("#fileUploadProgressTemplate").tmpl());
+    $("#fileUploadError").addClass("hide");
+
+    $.ajax({
+        url: '/echo/json/',
+        type: 'POST',
+        xhr: function() {
+            var xhr = $.ajaxSettings.xhr();
+            if (xhr.upload) {
+                xhr.upload.addEventListener('progress', function(evt) {
+                    var percent = (evt.loaded / evt.total) * 100;
+                    $("#files").find(".progress-bar").width(percent + "%");
+                }, false);
+            }
+            return xhr;
+        },
+        success: function(data) {
+            $("#files").children().last().remove();
+            $("#files").append($("#fileUploadItemTemplate").tmpl(data));
+            $("#uploadFile").closest("form").trigger("reset");
+        },
+        error: function() {
+            $("#fileUploadError").removeClass("hide").text("An error occured!");
+            $("#files").children().last().remove();
+            $("#uploadFile").closest("form").trigger("reset");
+        },
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false
+    }, 'json');
+});
--- footer.php
+++ footer.php
@@ -38,6 +38,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script src="assets/js/slick.js"></script>
+<script src="assets/js/upload.js"></script>
 <script src="assets/js/scripts.js"></script>
 </body>
 </html>
