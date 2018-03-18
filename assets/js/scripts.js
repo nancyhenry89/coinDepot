4167b45ea27a3571a2e87ea3d0dea03015cd6bb7
nancyhenry89 <nancyhenry89@gmail.com>
2018-03-18 11:49:43 +0200

    Code clean

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  //scan doc init
-  //-------------------
-  canvas = document.getElementById("canvas");
-	anim_container = document.getElementById("animation_container");
-	dom_overlay_container = document.getElementById("dom_overlay_container");
-	var comp=AdobeAn.getComposition("DE98C274D1023E4A95FBA2096D33E218");
-	var lib=comp.getLibrary();
-	handleComplete({},comp);
-  //--------------------
+    //scan doc init
+    //-------------------
+    canvas = document.getElementById("canvas");
+    anim_container = document.getElementById("animation_container");
+    dom_overlay_container = document.getElementById("dom_overlay_container");
+    var comp = AdobeAn.getComposition("DE98C274D1023E4A95FBA2096D33E218");
+    var lib = comp.getLibrary();
+    handleComplete({}, comp);
+    //--------------------
     //logo go to home
     //______________________
     $('.logo *').click(function() {
@@ -132,30 +132,30 @@ $(document).ready(function() {
                 }, 4000);
             }
             if (id == "paymentMethods") {
-              $('body').removeClass('modal-open');
-              $('body').addClass('page-popup');
-              $('.modal').fadeOut();
-              $('.page-popup').fadeOut();
-              $('#' + id).fadeIn(500, function() {
-                  addModalOpen();
-                  $('#verifyData .scanning').show();
-                  $('#verifyData .scan-success').hide();
-              });
+                $('body').removeClass('modal-open');
+                $('body').addClass('page-popup');
+                $('.modal').fadeOut();
+                $('.page-popup').fadeOut();
+                $('#' + id).fadeIn(500, function() {
+                    addModalOpen();
+                    $('#verifyData .scanning').show();
+                    $('#verifyData .scan-success').hide();
+                });
 
-                        }
-                        if (id == "orderComplete") {
-                            $('.modal,.page-popup').hide();
-                            $('body').removeClass('modal-open');
-                            $('body').addClass('page-opened');
-                            $('#orderComplete').fadeIn();
-                            setTimeout(function() {
-                                $('#payment').modal('hide');
-                            }, 500);
-                            $('#home').hide();
-                            $('html, body').animate({
-                                scrollTop: ($('body').offset().top)
-                            }, 0)
-                        }
+            }
+            if (id == "orderComplete") {
+                $('.modal,.page-popup').hide();
+                $('body').removeClass('modal-open');
+                $('body').addClass('page-opened');
+                $('#orderComplete').fadeIn();
+                setTimeout(function() {
+                    $('#payment').modal('hide');
+                }, 500);
+                $('#home').hide();
+                $('html, body').animate({
+                    scrollTop: ($('body').offset().top)
+                }, 0)
+            }
             //--------------------------------------------------
             //mobile popups end
             //--------------------------------------------------
@@ -225,7 +225,7 @@ $(document).ready(function() {
                     scrollTop: ($('body').offset().top)
                 }, 0)
 
-            }else if (id == "verifyData") {
+            } else if (id == "verifyData") {
                 $('body').addClass('modal-open');
                 $('#verifyData').fadeIn();
                 $('html, body').animate({
@@ -236,19 +236,18 @@ $(document).ready(function() {
                     $('#verifyData .scan-success').fadeIn();
                 }, 4000);
 
-            }else  if (id == "paymentMethods") {
-              $('body').removeClass('modal-open');
-              $('body').addClass('page-popup');
-              $('.modal').fadeOut();
-              $('.page-popup').fadeOut();
-              $('#' + id).fadeIn(500, function() {
-                  addModalOpen();
-                  $('#verifyData .scanning').show();
-                  $('#verifyData .scan-success').hide();
-              });
+            } else if (id == "paymentMethods") {
+                $('body').removeClass('modal-open');
+                $('body').addClass('page-popup');
+                $('.modal').fadeOut();
+                $('.page-popup').fadeOut();
+                $('#' + id).fadeIn(500, function() {
+                    addModalOpen();
+                    $('#verifyData .scanning').show();
+                    $('#verifyData .scan-success').hide();
+                });
 
-                        }
-            else {
+            } else {
                 $('.modal').modal('hide');
                 $('.page-popup').modal('hide');
                 $('#bookNow .close , #bookNow .cancel').click(function() {
@@ -428,7 +427,6 @@ $(window).resize(function() {
 
 
 
-
 //file uploader
 $("#uploadFile").change(function() {
     var formData = new FormData();
@@ -470,55 +468,60 @@ $("#uploadFile").change(function() {
 
 //scan doc function
 //___________________
-function handleComplete(evt,comp) {
-	//This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-	var lib=comp.getLibrary();
-	var ss=comp.getSpriteSheet();
-	exportRoot = new lib.Scan_Docs();
-	stage = new lib.Stage(canvas);
-	//Registers the "tick" event listener.
-	fnStartAnimation = function() {
-		stage.addChild(exportRoot);
-		createjs.Ticker.setFPS(lib.properties.fps);
-		createjs.Ticker.addEventListener("tick", stage);
-	}
-	//Code to support hidpi screens and responsive scaling.
-	function makeResponsive(isResp, respDim, isScale, scaleType) {
-		var lastW, lastH, lastS=1;
-		window.addEventListener('resize', resizeCanvas);
-		resizeCanvas();
-		function resizeCanvas() {
-			var w = lib.properties.width, h = lib.properties.height;
-			var iw = window.innerWidth, ih=window.innerHeight;
-			var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;
-			if(isResp) {
-				if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {
-					sRatio = lastS;
-				}
-				else if(!isScale) {
-					if(iw<w || ih<h)
-						sRatio = Math.min(xRatio, yRatio);
-				}
-				else if(scaleType==1) {
-					sRatio = Math.min(xRatio, yRatio);
-				}
-				else if(scaleType==2) {
-					sRatio = Math.max(xRatio, yRatio);
-				}
-			}
-			canvas.width = w*pRatio*sRatio;
-			canvas.height = h*pRatio*sRatio;
-			canvas.style.width = dom_overlay_container.style.width = anim_container.style.width =  w*sRatio+'px';
-			canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h*sRatio+'px';
-			stage.scaleX = pRatio*sRatio;
-			stage.scaleY = pRatio*sRatio;
-			lastW = iw; lastH = ih; lastS = sRatio;
-			stage.tickOnUpdate = false;
-			stage.update();
-			stage.tickOnUpdate = true;
-		}
-	}
-	makeResponsive(true,'both',false,1);
-	AdobeAn.compositionLoaded(lib.properties.id);
-	fnStartAnimation();
+function handleComplete(evt, comp) {
+    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
+    var lib = comp.getLibrary();
+    var ss = comp.getSpriteSheet();
+    exportRoot = new lib.Scan_Docs();
+    stage = new lib.Stage(canvas);
+    //Registers the "tick" event listener.
+    fnStartAnimation = function() {
+        stage.addChild(exportRoot);
+        createjs.Ticker.setFPS(lib.properties.fps);
+        createjs.Ticker.addEventListener("tick", stage);
+    }
+    //Code to support hidpi screens and responsive scaling.
+    function makeResponsive(isResp, respDim, isScale, scaleType) {
+        var lastW, lastH, lastS = 1;
+        window.addEventListener('resize', resizeCanvas);
+        resizeCanvas();
+
+        function resizeCanvas() {
+            var w = lib.properties.width,
+                h = lib.properties.height;
+            var iw = window.innerWidth,
+                ih = window.innerHeight;
+            var pRatio = window.devicePixelRatio || 1,
+                xRatio = iw / w,
+                yRatio = ih / h,
+                sRatio = 1;
+            if (isResp) {
+                if ((respDim == 'width' && lastW == iw) || (respDim == 'height' && lastH == ih)) {
+                    sRatio = lastS;
+                } else if (!isScale) {
+                    if (iw < w || ih < h)
+                        sRatio = Math.min(xRatio, yRatio);
+                } else if (scaleType == 1) {
+                    sRatio = Math.min(xRatio, yRatio);
+                } else if (scaleType == 2) {
+                    sRatio = Math.max(xRatio, yRatio);
+                }
+            }
+            canvas.width = w * pRatio * sRatio;
+            canvas.height = h * pRatio * sRatio;
+            canvas.style.width = dom_overlay_container.style.width = anim_container.style.width = w * sRatio + 'px';
+            canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h * sRatio + 'px';
+            stage.scaleX = pRatio * sRatio;
+            stage.scaleY = pRatio * sRatio;
+            lastW = iw;
+            lastH = ih;
+            lastS = sRatio;
+            stage.tickOnUpdate = false;
+            stage.update();
+            stage.tickOnUpdate = true;
+        }
+    }
+    makeResponsive(true, 'both', false, 1);
+    AdobeAn.compositionLoaded(lib.properties.id);
+    fnStartAnimation();
 }
